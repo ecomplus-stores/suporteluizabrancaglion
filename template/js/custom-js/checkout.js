f1da987c4301107cdeeae48b4033705ba2a77090
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-08-13 09:31:43 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,2 +1,12 @@
-// Add your custom JavaScript for checkout here.
- 
+// Adiciona o container da grade de avaliações
+const reviewsDiv = document.createElement('div');
+reviewsDiv.className = 'sk-ww-google-reviews';
+reviewsDiv.setAttribute('data-embed-id', '25587705');
+document.body.appendChild(reviewsDiv); // ou escolha um container específico
+
+// Adiciona dinamicamente o script SociableKIT
+const script = document.createElement('script');
+script.src = 'https://widgets.sociablekit.com/google-reviews/widget.js';
+script.defer = true;
+document.body.appendChild(script);
+

736cac7eecc6a33428913615b4a44af310cc05e4
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-12-19 10:43:34 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -26,20 +26,39 @@ function checkAndLoadWidget() {
   }
 }
 
-document.addEventListener('DOMContentLoaded', checkAndLoadWidget);
-window.addEventListener('hashchange', checkAndLoadWidget);
 storefront.on('widget:@ecomplus/widget-tag-manager', function () {
-    document.querySelector('.cart__btn-checkout').insertAdjacentHTML('beforebegin', `
-    <div id="block-confirm" class="form-group"><div class="custom-control custom-checkbox"><input type="checkbox" id="input-confirm-checkout" class="custom-control-input"> <label for="input-confirm-checkout" class="custom-control-label">
-         Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados 
-          entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026, 
-          e o prazo de entrega passará a contar a partir dessa data.
-        </label></div></div>
-    `);
-    document.querySelector('#block-confirm').addEventListener('click', (e) => {
-        if (e.target.checked) {
-            document.querySelector('#block-confirm').style.display = 'none'
-            document.querySelector('.cart__btn-checkout').style.display = 'block'
+  // espera o DOM do checkout
+  var checkDomInterval = setInterval(function () {
+    var checkoutBtn = document.querySelector('.cart__btn-checkout')
+
+    if (checkoutBtn) {
+      clearInterval(checkDomInterval)
+
+      // esconde o botão até marcar o checkbox
+      checkoutBtn.style.display = 'none'
+
+      checkoutBtn.insertAdjacentHTML('beforebegin', `
+        <div id="block-confirm" class="form-group">
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" id="input-confirm-checkout" class="custom-control-input">
+            <label for="input-confirm-checkout" class="custom-control-label">
+              Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados 
+              entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026, 
+              e o prazo de entrega passará a contar a partir dessa data.
+            </label>
+          </div>
+        </div>
+      `)
+
+      var confirmInput = document.querySelector('#input-confirm-checkout')
+
+      // quando marcar, mostra o botão e remove o bloco
+      confirmInput.addEventListener('change', function () {
+        if (confirmInput.checked) {
+          document.querySelector('#block-confirm').remove()
+          checkoutBtn.style.display = 'block'
         }
-    })
+      })
+    }
+  }, 300)
 })
